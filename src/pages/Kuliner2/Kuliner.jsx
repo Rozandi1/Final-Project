0f488b3c301874fa980b67fcb3f0697c382b9fa1
Dila <lilafadila64@gmail.com>
2022-04-10 14:34:40 +0700

    fix kuliner page

--- src/pages/Kuliner2/Kuliner.jsx
+++ src/pages/Kuliner2/Kuliner.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
 import Navbar from '../../component/nav/Navbar'
+// import Singang from '../../assets/Singang.jpg'
+import React, {useState, useEffect} from 'react'
+import '../Kuliner2/Kuliner.css'
+import Footer from '../../component/foot/Footer'
 
 function Kuliner() {
+    const [kuliner, setKuliner] = useState([])
+    async function fetchKuliner() {
+      fetch('https://west-broad-gerbil.glitch.me/kuliner')
+      .then(response => response.json())
+      .then(data => setKuliner(data))
+      .catch(err => console.log(err))
+    }
+  
+    useEffect(()=> {
+        fetchKuliner()
+    },[])
   return (
     <>
         <Navbar/>
@@ -19,17 +33,32 @@ function Kuliner() {
       </div>
     </section>
 
-    <section id='bannerKuliner'>
-    <div className="container-fluid my-3" style={{backgroundColor: '#056ccc'}}>
+    {/* card kuliner */}
+     {/* kuliner Card */}
+     <section id='kulinerCard'>
+      <div className="container-fluid">
         <div className="container">
-            <div className="row">
-                <div className="col">
-                <h1>halo</h1>
+          <div className="row justify-content-center ">
+            {
+              kuliner.map(kuliner => {
+                return <div className="col-md-3 col-12 mt-3" key={kuliner.id} >
+                <div class="card h-100 border-0 shadow p-2" style={{borderRadius: '5%'}}>
+                <img src={kuliner.image} class="img-responsive" alt="..." style={{height: '200px', objectFit: 'cover', objectPosition: 'center center', borderRadius: '5%'}} />
+                <div class="card-body">
+                        <h5 class="card-title">{kuliner.name}</h5>
+                        <p class="card-text">{kuliner.price}</p>
+                        <button id='buttonDesc' href="#" class="btn shadow border-0 text-white" style={{borderRadius: '8px', backgroundColor: '#056ccc'}}>Go somewhere</button>
+                      </div>
+                    </div>
                 </div>
-            </div>
+              })
+            }
+            {/* end row 1 */}
+          </div>
         </div>
-    </div>
+      </div>
     </section>
+    <Footer/>
     </> 
   )
 }
